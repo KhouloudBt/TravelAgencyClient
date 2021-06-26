d6d9bc13c8b09ff07bd1b4b7b20019a2f529abac
KhouloudBt <khouloud.bentaoues33@gmail.com>
2021-06-26 13:57:35 +0100

    lastEK

--- routes/admin.js
+++ routes/admin.js
@@ -2,34 +2,17 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch'); 
 const session = require('express-session'); 
-const auth = require ('./')  ;
+const adminService = require ('./services/adminService')  ;
 
 const Bluebird = require('bluebird');
  
 fetch.Promise = Bluebird;
 
-
-function getNumberEmployees()
-{
-    return (fetch("http://localhost:3002/personnel/getNumber")
-    .then(res=>res.json()))
-}
-
-function getNumberTrips()
-{
-    return (fetch("http://localhost:3002/trips/getNumber")
-    .then(res=>res.json()))
-}
-/* GET home page. */
 router.get('/', async function(req, res, next) {
-  console.log();
-  let nbEmp = await getNumberEmployees();
-  let nbTrips = await getNumberTrips();
-
-  console.log(nbEmp);
-  res.render('admin/home', { nbEmp: nbEmp.number, nbTrips:nbTrips.number});
+  if (req.session.user != null && req.session.user.id_role==2)
+  adminService.redirectHome(req,res);
+  else 
+  adminService.redirectLogin(req,res);
 });
-
-
 
 module.exports = router;
